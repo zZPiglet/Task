c8c3f8a3ab3572a89eddc30613da4257cf25a884
zZPiglet <zZPiglet@users.noreply.github.com>
2020-04-24 18:39:44 +0800

    Update

--- backup/lkyl.js
+++ backup/lkyl.js
@@ -13,9 +13,11 @@ by Macsuny
 ~~~~~~~~~~~~~~~~
 Surge 4.0 :
 [Script]
-cron "0 9 * * *" script-path=https://raw.githubusercontent.com/Sunert/Scripts/master/Task/lkyl.js
+lkyl.js = type=cron,cronexp=35 5 0 * * *,script-path=https://raw.githubusercontent.com/Sunert/Scripts/master/Task/lkyl.js,script-update-interval=0
+
 # 来客有礼 Cookie.
-http-request https:\/\/draw\.jdfcloud\.com\/\/api\/turncard\/sign\? script-path=https://raw.githubusercontent.com/Sunert/Scripts/master/Task/lkyl.js
+lkyl.js = script-path=https://raw.githubusercontent.com/Sunert/Scripts/master/Task/lkyl.js,type=http-request,pattern=https:\/\/draw\.jdfcloud\.com\/\/api\/turncard\/sign\?
+
 ~~~~~~~~~~~~~~~~
 QX 1.0.5+ :
 [task_local]
@@ -38,90 +40,247 @@ const signheaderVal = sy.getdata(signheaderKey)
 
 let isGetCookie = typeof $request !== 'undefined'
 if (isGetCookie) {
-   GetCookie()
+    GetCookie()
 } else {
-   sign()
+    sign()
 }
 function GetCookie() {
-const requrl = $request.url
-if ($request && $request.method != 'OPTIONS') {
-  const signurlVal = requrl
-  const signheaderVal = JSON.stringify($request.headers)
-  sy.log(`signurlVal:${signurlVal}`)
-  sy.log(`signheaderVal:${signheaderVal}`)
-  if (signurlVal) sy.setdata(signurlVal, signurlKey)
-  if (signheaderVal) sy.setdata(signheaderVal, signheaderKey)
-  sy.msg(cookieName, `获取Cookie: 成功🎉`, ``)
-  }
- }
-
+    const requrl = $request.url
+    if ($request && $request.method != 'OPTIONS') {
+        const signurlVal = requrl
+        const signheaderVal = JSON.stringify($request.headers)
+        const cookieVal = $request.headers['Cookie'];
+        sy.log(`signurlVal:${signurlVal}`)
+        sy.log(`signheaderVal:${signheaderVal}`)
+        if (signurlVal) sy.setdata(signurlVal, signurlKey)
+        if (signheaderVal) sy.setdata(signheaderVal, signheaderKey)
+        sy.msg(cookieName, `获取Cookie: 成功🎉`, ``)
+    }
+}
+const token = JSON.parse(signheaderVal)
+const openid = token['openId']
+const appid = token['App-Id']
 function sign() {
-	  let signurl = {
-		url: signurlVal,
-		headers: JSON.parse(signheaderVal)
-	}
-    sy.post(signurl, (error, response, data) => {
-      sy.log(`${cookieName}, data: ${data}`)
-      let result = JSON.parse(data)
-      const title = `${cookieName}`
-      let detail = ``
-      let subTitle = ``
-   
-     if (result.success == true) {
-      subTitle = `签到结果: 成功🎉`
-      detail = `${result.data.topLine},${result.data.rewardName},获得京豆: ${result.data.jdBeanQuantity}`
-      } else if (result.errorMessage == `今天已经签到过了哦`) {
-      subTitle = `签到结果: 重复`
-      detail = `说明: ${result.errorMessage}!`
-      } else  {
-      subTitle = `签到结果: 失败`
-      detail = `说明: ${result.errorMessage}`
-      }
-      sy.msg(title, subTitle, detail)
-     })
-  }
+    return new Promise((resolve, reject) =>{
+        let signurl = {
+            url: signurlVal,
+            headers: JSON.parse(signheaderVal)
+        }
+        sy.post(signurl, (error, response, data) => {
+            sy.log(`${cookieName}, data: ${data}`)
+            let result = JSON.parse(data)
+            const title = `${cookieName}`
+            if (result.success == true) {
+                subTitle = `签到结果: 成功🎉`
+                detail = `${result.data.topLine},${result.data.rewardName},获得京豆: ${result.data.jdBeanQuantity}`
+            } else if (result.errorMessage == `今天已经签到过了哦`) {
+                subTitle = `签到结果: 重复‼️`
+                detail = `说明: ${result.errorMessage}!`
 
+            } else  {
+                subTitle = `签到结果: 失败`
+                detail = `说明: ${result.errorMessage}`
+            }
+            lottery(),
+                resolve()
+        })
+    })
+}
 
+function lottery() {
+    return new Promise((resolve, reject) =>{
+        let daytaskurl = {
+            url: `https://draw.jdfcloud.com//api/bean/square/getTaskInfo?openId=${openid}&taskCode=lottery&appId=${appid}`,
+            headers: JSON.parse(signheaderVal)
+        }
+        daytaskurl.headers['Content-Length'] = `0`;
+        sy.get(daytaskurl, (error, response, data) => {
+            //sy.log(`${cookieName}, data: ${data}`)
+            let result = JSON.parse(data)
+            if (result.success == true) {
+                //detail += `\n今日抽奖获取银豆: ${result.data.rewardAmount}`
+            }
+            status()
+            resolve()
+        })
+    })
+}
+
+function status() {
+    return new Promise((resolve, reject) =>{
+        let statusurl = {
+            url: `https://draw.jdfcloud.com//api/bean/square/silverBean/task/get?openId=${openid}&appId=${appid}`,
+            headers: JSON.parse(signheaderVal),
+        }
+        statusurl.headers['Content-Length'] = `0`;
+        sy.get(statusurl, (error, response, data) => {
+            sy.log(`${cookieName}, data: ${data}`)
+            let result = JSON.parse(data)
+            if (result.success == true) {
+                //detail += ``
+            }
+            video()
+            resolve()
+        })
+    })
+}
+
+function video() {
+    return new Promise((resolve, reject) =>{
+        let videourl = {
+            url: `https://draw.jdfcloud.com//api/bean/square/silverBean/task/join?appId=${appid}`,
+            headers: JSON.parse(signheaderVal),
+            body: `{"openId": "['openid']","taskCode": "watch_video"}`}
+        videourl.headers['Content-Length'] = `0`;
+        sy.post(videourl, (error, response, data) => {
+            sy.log(`${cookieName}, data: ${data}`)
+            let result = JSON.parse(data)
+            if (result.success == true) {
+                //detail += `\n`
+            }
+            let videotaskurl = {
+                url: `https://draw.jdfcloud.com//api/bean/square/silverBean/taskReward/get?openId=${openid}&taskCode=watch_video&inviterOpenId=&appId=${appid}`,
+                headers: JSON.parse(signheaderVal)
+            }
+            videotaskurl.headers['Content-Length'] = `0`;
+            sy.get(videotaskurl, (error, response, data) => {
+                sy.log(`${cookieName}, data: ${data}`)
+                let result = JSON.parse(data)
+                if (result.success == true) {
+                    //detail += `\n`
+                }
+            })
+            award()
+            resolve()
+        })
+    })
+}
+
+function award() {
+    return new Promise((resolve, reject) =>{
+        let weektaskurl = {
+            url: `https://draw.jdfcloud.com//api/lottery/home/v2?openId=${openid}&appId=${appid}`,
+            headers: JSON.parse(signheaderVal)
+        }
+        weektaskurl.headers['Content-Length'] = `0`;
+        sy.get(weektaskurl, (error, response, data) => {
+            sy.log(`${cookieName}, data: ${data}`)
+            let result = JSON.parse(data)
+            if (result.success == true) {
+                detail += `  您已参与${result.data.homeActivities.length}个抽奖`
+                for (i=0;i < 3;i++)
+                {
+                    lotteryId = result.data.homeActivities[i].activityId
+                    let awardurl = {
+                        url: `https://draw.jdfcloud.com//api/lottery/participate?lotteryId=${lotteryId}&openId=${openid}&formId=123&source=HOME&appId=${appid}`,
+                        headers: JSON.parse(signheaderVal)
+                    }
+                    sy.post(awardurl, (error, response, data) =>
+                    {
+                        sy.log(`${cookieName}, data: ${data}`)
+                    })
+                }
+            }
+            bean()
+            resolve()
+        })
+    })
+}
+function bean() {
+    return new Promise((resolve, reject) => {
+        let beanurl = {
+            url: `https://draw.jdfcloud.com//api/lottery/risk?relatedIdType=BEAN_SQUARE_ACTIVE_ID&relatedId=1&appId=${appid}`,
+            headers: JSON.parse(signheaderVal)
+        }
+        beanurl.headers['Content-Length'] = `0`;
+        sy.post(beanurl, (error, response, data) =>
+        {
+            sy.log(`${cookieName}, data: ${data}`)
+        })
+        total()
+        resolve()
+    })
+}
+function total() {
+    return new Promise((resolve, reject) =>{
+        let lotteryurl = {
+            url: `https://draw.jdfcloud.com//api/bean/square/silverBean/getUserBalance?openId=${openid}&appId=${appid}`,
+            headers: JSON.parse(signheaderVal)
+        }
+        lotteryurl.headers['Content-Length'] = `0`;
+        sy.get(lotteryurl, (error, response, data) => {
+            sy.log(`${cookieName}, data: ${data}`)
+            let result = JSON.parse(data)
+            if (result.success == true) {
+                SilverBean = `${result.data}`
+                detail += `\n您共计${SilverBean}个银豆`
+            }
+            let hinturl = {
+                url: `https://draw.jdfcloud.com//api/bean/square/silverBean/getJdBeanList?openId=${openid}&appId=${appid}`,
+                headers: JSON.parse(signheaderVal)
+            }
+            hinturl.headers['Content-Length'] = `0`;
+            sy.get(hinturl, (error, response, data) => {
+                sy.log(`${cookieName}, data: ${data}`)
+                let result = JSON.parse(data)
+                const title = `${cookieName}`
+                if (SilverBean >= 20) {
+                    for (k=0; k < result.datas.length;k++){
+                        if (result.datas[k].salePrice >= SilverBean && SilverBean > result.datas[k-1].salePrice)
+                        {
+                            detail += `   可兑换: ${result.datas[k-1].memo}`
+                        }
+                    }
+                } else if (SilverBean < 20)
+                {
+                    detail += `  银豆不足以兑换京豆`
+                }
+                sy.msg(title, subTitle, detail)
+            })
+        })
+        resolve()
+    })
+}
 function init() {
-  isSurge = () => {
-    return undefined === this.$httpClient ? false : true
-  }
-  isQuanX = () => {
-    return undefined === this.$task ? false : true
-  }
-  getdata = (key) => {
-    if (isSurge()) return $persistentStore.read(key)
-    if (isQuanX()) return $prefs.valueForKey(key)
-  }
-  setdata = (key, val) => {
-    if (isSurge()) return $persistentStore.write(key, val)
-    if (isQuanX()) return $prefs.setValueForKey(key, val)
-  }
-  msg = (title, subTitle, body) => {
-    if (isSurge()) $notification.post(title, subTitle, body)
-    if (isQuanX()) $notify(title, subTitle, body)
-  }
-  log = (message) => console.log(message)
-  get = (url, cb) => {
-    if (isSurge()) {
-      $httpClient.get(url, cb)
+    isSurge = () => {
+        return undefined === this.$httpClient ? false : true
     }
-    if (isQuanX()) {
-      url.method = 'GET'
-      $task.fetch(url).then((resp) => cb(null, resp, resp.body))
+    isQuanX = () => {
+        return undefined === this.$task ? false : true
     }
-  }
-  post = (url, cb) => {
-    if (isSurge()) {
-      $httpClient.post(url, cb)
+    getdata = (key) => {
+        if (isSurge()) return $persistentStore.read(key)
+        if (isQuanX()) return $prefs.valueForKey(key)
     }
-    if (isQuanX()) {
-      url.method = 'POST'
-      $task.fetch(url).then((resp) => cb(null, resp, resp.body))
+    setdata = (key, val) => {
+        if (isSurge()) return $persistentStore.write(key, val)
+        if (isQuanX()) return $prefs.setValueForKey(key, val)
     }
-  }
-  done = (value = {}) => {
-    $done(value)
-  }
-  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
+    msg = (title, subTitle, body) => {
+        if (isSurge()) $notification.post(title, subTitle, body)
+        if (isQuanX()) $notify(title, subTitle, body)
+    }
+    log = (message) => console.log(message)
+    get = (url, cb) => {
+        if (isSurge()) {
+            $httpClient.get(url, cb)
+        }
+        if (isQuanX()) {
+            url.method = 'GET'
+            $task.fetch(url).then((resp) => cb(null, resp, resp.body))
+        }
+    }
+    post = (url, cb) => {
+        if (isSurge()) {
+            $httpClient.post(url, cb)
+        }
+        if (isQuanX()) {
+            url.method = 'POST'
+            $task.fetch(url).then((resp) => cb(null, resp, resp.body))
+        }
+    }
+    done = (value = {}) => {
+        $done(value)
+    }
+    return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
 }
