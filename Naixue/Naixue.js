af682e89749fdda23b7111c5ad3eebec17e1f33c
zZPiglet <zZPiglet@users.noreply.github.com>
2020-05-25 01:59:30 +0800

    微信小程序"奈雪点单"自动签到

--- Naixue/Naixue.js
+++ Naixue/Naixue.js
@@ -13,7 +13,7 @@ or remote
 [rewrite_local]
 ^https:\/\/webapi\.qmai\.cn\/web\/cy\/v\d\/store\/template-scene url script-request-header Naixue.js
 or remote
-^https:\/\/webapi\.qmai\.cn\/web\/cy\/v\d\/store\/template-scene https://raw.githubusercontent.com/zZPiglet/Task/master/Naixue/Naixue.js
+^https:\/\/webapi\.qmai\.cn\/web\/cy\/v\d\/store\/template-scene url script-request-header https://raw.githubusercontent.com/zZPiglet/Task/master/Naixue/Naixue.js
 
 
 Surge 4.0+:
