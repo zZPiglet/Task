c58473d269448533d40c84bd7acb9fcc596f69a3
zZPiglet <zZPiglet@users.noreply.github.com>
2020-06-12 01:16:44 +0800

    对按照 DiDi.js 正确配置但无法获取 token 且"滴滴出行"主页右上角有"天天领福利"的用户适用的 token 获取脚本

---
+++ DiDi/DiDi_ticket.js
@@ -0,0 +1,180 @@
+/*
+本脚本适用于按照 DiDi.js 正确配置但无法获取 token 且"滴滴出行"主页右上角有"天天领福利"的用户，用此脚本获取 token 后请用回 DiDi.js 进行定时任务。
+请先按下述方法进行配置，进入"滴滴出行"，点击右上角"天天领福利"，若弹出"首次写入滴滴出行 Token 成功"即可正常食用，其他提示或无提示请发送日志信息至 issue。
+
+⚠️免责声明：
+1. 此脚本仅用于学习研究，不保证其合法性、准确性、有效性，请根据情况自行判断，本人对此不承担任何保证责任。
+2. 由于此脚本仅用于学习研究，您必须在下载后 24 小时内将所有内容从您的计算机或手机或任何存储设备中完全删除，若违反规定引起任何事件本人对此均不负责。
+3. 请勿将此脚本用于任何商业或非法目的，若违反规定请自行对此负责。
+4. 此脚本涉及应用与本人无关，本人对因此引起的任何隐私泄漏或其他后果不承担任何责任。
+5. 本人对任何脚本引发的问题概不负责，包括但不限于由脚本错误引起的任何损失和损害。
+6. 如果任何单位或个人认为此脚本可能涉嫌侵犯其权利，应及时通知并提供身份证明，所有权证明，我们将在收到认证文件确认后删除此脚本。
+7. 所有直接或间接使用、查看此脚本的人均应该仔细阅读此声明。本人保留随时更改或补充此声明的权利。一旦您使用或复制了此脚本，即视为您已接受此免责声明。
+
+Author：zZPiglet
+
+Quantumult X (App Store:1.0.5+, TestFlight 190+):
+[task_local]
+1 0 * * * DiDi.js, tag=滴滴出行
+or remote
+1 0 * * * https://raw.githubusercontent.com/zZPiglet/Task/master/DiDi/DiDi.js, tag=滴滴出行
+
+[rewrite_local]
+^https:\/\/bosp-api\.xiaojukeji\.com\/wechat\/benefit\/public\/index\? url script-request-header DiDi_ticket.js
+or remote
+^https:\/\/bosp-api\.xiaojukeji\.com\/wechat\/benefit\/public\/index\? url script-request-header https://raw.githubusercontent.com/zZPiglet/Task/master/DiDi/DiDi_ticket.js
+
+
+Surge 4.0+ & Loon:
+[Script]
+cron "1 0 * * *" script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/DiDi/DiDi.js
+http-request ^https:\/\/bosp-api\.xiaojukeji\.com\/wechat\/benefit\/public\/index\? script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/DiDi/DiDi_ticket.js
+
+All app:
+[mitm]
+hostname = bosp-api.xiaojukeji.com
+
+获取完 Token 后可不注释 rewrite / hostname，Token 更新时会弹窗。若因 MitM 导致该软件或小程序网络不稳定，可注释掉 hostname。
+*/
+
+const TokenName = '滴滴出行'
+const TokenKey = 'DiDi'
+const CityKey = 'DiDi_city'
+const reg = /^https:\/\/bosp-api\.xiaojukeji\.com\/wechat\/benefit\/public\/index\?(.*)%22token%22:%22(.*)%22(.*)&city_id=(\d*)&/
+const $cmp = compatibility()
+GetToken()
+$cmp.done()
+
+function GetToken() {
+    if ($request) {
+        const requrl = $request.url
+        let TokenValue = decodeURIComponent(reg.exec(requrl)[2])
+        let CityValue = reg.exec(requrl)[4]
+        $cmp.write(CityValue, CityKey)
+        if ($cmp.read(TokenKey) != (undefined || null)) {
+            if ($cmp.read(TokenKey) != TokenValue) {
+                let token = $cmp.write(TokenValue, TokenKey)
+                if (!token) {
+                    $cmp.notify("更新" + TokenName + " Token 失败‼️", "", "")
+                } else {
+                    $cmp.notify("更新" + TokenName + " Token 成功 🎉", "", "")
+                }
+            }
+        } else {
+            let token = $cmp.write(TokenValue, TokenKey)
+            if (!token) {
+                $cmp.notify("首次写入" + TokenName + " Token 失败‼️", "", "")
+            } else {
+                $cmp.notify("首次写入" + TokenName + " Token 成功 🎉", "", "")
+            }
+        }
+    } else {
+        $cmp.notify("写入" + TokenName + "Token 失败‼️", "", "配置错误, 无法读取请求头, ")
+    }
+}
+
+function compatibility() {
+    const isRequest = typeof $request != "undefined"
+    const isSurge = typeof $httpClient != "undefined"
+    const isQuanX = typeof $task != "undefined"
+    const isJSBox = typeof $app != "undefined" && typeof $http != "undefined"
+    const isNode = typeof require == "function" && !isJSBox
+    const node = (() => {
+        if (isNode) {
+            const request = require('request')
+            return ({request})
+        } else {
+            return (null)
+        }
+    })()
+    const notify = (title, subtitle, message) => {
+        if (isQuanX) $notify(title, subtitle, message)
+        if (isSurge) $notification.post(title, subtitle, message)
+        if (isNode) log(title+subtitle+message)
+        if (isJSBox) $push.schedule({title: title, body: subtitle?subtitle+"\n"+message:message})
+    }
+    const write = (value, key) => {
+        if (isQuanX) return $prefs.setValueForKey(value, key)
+        if (isSurge) return $persistentStore.write(value, key)
+    }
+    const read = (key) => {
+        if (isQuanX) return $prefs.valueForKey(key)
+        if (isSurge) return $persistentStore.read(key)
+    }
+    const adapterStatus = (response) => {
+        if (response) {
+            if (response.status) {
+                response["statusCode"] = response.status
+            } else if (response.statusCode) {
+                response["status"] = response.statusCode
+            }
+        }
+        return response
+    }
+    const get = (options, callback) => {
+        if (isQuanX) {
+            if (typeof options == "string") options = { url: options }
+            options["method"] = "GET"
+            $task.fetch(options).then(response => {
+                callback(null, adapterStatus(response), response.body)
+            }, reason => callback(reason.error, null, null))
+        }
+        if (isSurge) $httpClient.get(options, (error, response, body) => {
+            callback(error, adapterStatus(response), body)
+        })
+        if (isNode) {
+            node.request(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
+        if (isJSBox) {
+            if (typeof options == "string") options = {url: options}
+            options["header"] = options["headers"]
+            options["handler"] = function (resp) {
+                let error = resp.error
+                if (error) error = JSON.stringify(resp.error)
+                let body = resp.data
+                if (typeof body == "object") body = JSON.stringify(resp.data)
+                callback(error, adapterStatus(resp.response), body)
+            }
+            $http.get(options)
+        }
+    }
+    const post = (options, callback) => {
+        if (isQuanX) {
+            if (typeof options == "string") options = { url: options }
+            options["method"] = "POST"
+            $task.fetch(options).then(response => {
+                callback(null, adapterStatus(response), response.body)
+            }, reason => callback(reason.error, null, null))
+        }
+        if (isSurge) {
+            $httpClient.post(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
+        if (isNode) {
+            node.request.post(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
+        if (isJSBox) {
+            if (typeof options == "string") options = {url: options}
+            options["header"] = options["headers"]
+            options["handler"] = function (resp) {
+                let error = resp.error
+                if (error) error = JSON.stringify(resp.error)
+                let body = resp.data
+                if (typeof body == "object") body = JSON.stringify(resp.data)
+                callback(error, adapterStatus(resp.response), body)
+            }
+            $http.post(options)
+        }
+    }
+    const log = (message) => console.log(message)
+    const done = (value = {}) => {
+        if (isQuanX) isRequest ? $done(value) : null
+        if (isSurge) isRequest ? $done(value) : $done()
+    }
+    return { isQuanX, isSurge, isJSBox, isRequest, notify, write, read, get, post, log, done }
+}
