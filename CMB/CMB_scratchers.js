cb797098a95d7193967066bf8673142eb6cd5d4f
zZPiglet <zZPiglet@users.noreply.github.com>
2020-07-12 21:54:28 +0800

    招商银行-招牌便民刮刮乐半自动

---
+++ CMB/CMB_scratchers.js
@@ -0,0 +1,25 @@
+/*
+qx(tf 1.0.11(316)) 、 loon(tf 2.1.1(163)) 及更新版本可用。
+半自动提醒完成招行便民刮刮乐活动。
+0 23 * * *  （挑一个玩手机的时间）
+到时候点击通知即可跳转至招行便民刮刮乐界面，完成签到刮奖即可。
+
+自用 by zZPiglet
+*/
+
+const $ = new compatibility()
+
+const cmb_scratchers = "cmbmobilebank://cmbls/functionjump?action=gocorpno&corpno=100891&shorturl=https%3A%2F%2Fcmb-scratchers.weijuju.com%2Fapp%2Findex"
+
+$.notify("招商银行-招牌便民刮刮乐", "", "点击跳转去签到刮奖啦", cmb_scratchers)
+
+$done()
+
+function compatibility() {
+    _isQuanX = typeof $task != "undefined"
+    _isLoon = typeof $loon != "undefined"
+    this.notify = (title, subtitle, message, url) => {
+        if (_isLoon) $notification.post(title, subtitle, message, url)
+        if (_isQuanX) $notify(title, subtitle, message, {"open-url" : url})
+    }
+}
