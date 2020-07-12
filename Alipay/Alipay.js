9bfd8193463e8cc6b18486f8ef10eb86e69dded2
zZPiglet <zZPiglet@users.noreply.github.com>
2020-07-12 23:59:51 +0800

    接入 BoxJs，以方便远程更改间隔时间

--- Alipay/Alipay.js
+++ Alipay/Alipay.js
@@ -1,15 +1,15 @@
 /*
-qx 及 loon 最新 tf 可用。
+qx(tf 1.0.11(316)) 、 loon(tf 2.1.1(163)) 及更新版本可用。
 半自动提醒支付宝签到及蚂蚁森林收能量。
 25 7 * * * Alipay.js
 到时候点击通知即可跳转领积分，领完积分等待一定延迟（可自行设置符合自己手速的）后点击第二个通知跳转收能量。
+可在 boxjs 中设置两个通知之间的时间间隔。
 
 自用 by zZPiglet
 */
 
-const delay = 8000 //设置两个通知之前的延迟，单位毫秒
-
-const $ = new cmp()
+const $ = new compatibility()
+const delay = $.read('Alipay_wait_mayi') * 1000 || 8000
 const point = "alipays://platformapi/startapp?appId=20000160&url=/www/myPoints.html"
 const mayi = "alipay://platformapi/startapp?appId=60000002"
 
@@ -21,9 +21,13 @@ setTimeout(() => {
 
 $done()
 
-function cmp() {
+function compatibility() {
     _isQuanX = typeof $task != "undefined"
     _isLoon = typeof $loon != "undefined"
+    this.read = (key) => {
+        if (_isQuanX) return $prefs.valueForKey(key)
+        if (_isLoon) return $persistentStore.read(key)
+    }
     this.notify = (title, subtitle, message, url) => {
         if (_isLoon) $notification.post(title, subtitle, message, url)
         if (_isQuanX) $notify(title, subtitle, message, {"open-url" : url})
