b383ed7b2515b019a90a83c5eb8865b383c71418
zZPiglet <zZPiglet@users.noreply.github.com>
2020-09-24 00:23:17 +0800

    新增指令 zgd，通过 https://zhao.pp.ua/ 查找 Google Drive 内容。

--- Q-Search.js
+++ Q-Search.js
@@ -15,82 +15,90 @@ hostname = duckduckgo.com
 
 const engineData = {
     // Wikipedia
-    "wiki": "https://wikipedia.org/wiki/%@",
+    wiki: "https://wikipedia.org/wiki/%@",
     // Wikipedia 中文
-    "wk": "https://zh.wikipedia.org/wiki/%@",
+    wk: "https://zh.wikipedia.org/wiki/%@",
     // Magi
-    "mg": "https://magi.com/search?q=%@",
+    mg: "https://magi.com/search?q=%@",
     // 百度
-    "bd": "https://www.baidu.com/s?wd=%@",
+    bd: "https://www.baidu.com/s?wd=%@",
     // GitHub
-    "gh": "https://github.com/search?q=%@",
+    gh: "https://github.com/search?q=%@",
     // Google 搜索 TestFlight
-    "tf": "https://www.google.com/search?as_q=%@&as_sitesearch=testflight.apple.com",
+    tf: "https://www.google.com/search?as_q=%@&as_sitesearch=testflight.apple.com",
     // Google 图片
-    "gi": "https://www.google.com/search?&tbm=isch&q=%@",
+    gi: "https://www.google.com/search?&tbm=isch&q=%@",
     // 有道词典
-    "yd": "https://dict.youdao.com/search?q=%@",
+    yd: "https://dict.youdao.com/search?q=%@",
     // Google 译至中
-    "trc": "https://translate.google.com/#view=home&op=translate&sl=auto&tl=zh-CN&text=%@",
+    trc: "https://translate.google.com/#view=home&op=translate&sl=auto&tl=zh-CN&text=%@",
     // Google 译至英
-    "tre": "https://translate.google.com/#view=home&op=translate&sl=auto&tl=en&text=%@",
+    tre: "https://translate.google.com/#view=home&op=translate&sl=auto&tl=en&text=%@",
     // Google 译至日
-    "trj": "https://translate.google.com/#view=home&op=translate&sl=auto&tl=ja&text=%@",
+    trj: "https://translate.google.com/#view=home&op=translate&sl=auto&tl=ja&text=%@",
     // 少数派站内搜索
-    "sspai": "https://sspai.com/search/post/%@",
+    sspai: "https://sspai.com/search/post/%@",
     // Google 搜索少数派
-    "ssp": "https://www.google.com/search?as_q=%@&as_sitesearch=sspai.com",
+    ssp: "https://www.google.com/search?as_q=%@&as_sitesearch=sspai.com",
     // YouTube
-    "ytb": "https://www.youtube.com/results?search_query=%@",
+    ytb: "https://www.youtube.com/results?search_query=%@",
     // Stack Overflow
-    "so": "https://stackoverflow.com/search?q=%@",
+    so: "https://stackoverflow.com/search?q=%@",
     // StackExchange
-    "se": "https://stackexchange.com/search?q=%@",
+    se: "https://stackexchange.com/search?q=%@",
     // WolframAlpha
-    "wa": "https://www.wolframalpha.com/input/?i=%@",
+    wa: "https://www.wolframalpha.com/input/?i=%@",
     // 豆瓣
-    "db": "https://www.douban.com/search?q=%@",
+    db: "https://www.douban.com/search?q=%@",
     // 知乎
-    "zh": "https://www.zhihu.com/search?q=%@",
+    zh: "https://www.zhihu.com/search?q=%@",
     // 微博
-    "wb": "https://s.weibo.com/weibo/%@",
+    wb: "https://s.weibo.com/weibo/%@",
     // Reddit
-    "rd": "https://www.reddit.com/search?q=%@",
+    rd: "https://www.reddit.com/search?q=%@",
     // Twitter
-    "tw": "https://twitter.com/search?q=%@",
+    tw: "https://twitter.com/search?q=%@",
     // Google 搜索 Google Drive 资源
-    "gd": "https://www.google.com/search?q=%22Google+Drive%22+%@",
+    gd: "https://www.google.com/search?q=%22Google+Drive%22+%@",
     // t.me/gdurl 搜索 Google Drive 资源
-    "tgd": "https://t.me/s/gdurl?q=%@",
+    tgd: "https://t.me/s/gdurl?q=%@",
     // t.me/Remux_2160P 搜索 Google Drive 资源
-    "t4k": "https://t.me/s/Remux_2160P?q=%@\n",
+    t4k: "https://t.me/s/Remux_2160P?q=%@",
+    // https://zhao.pp.ua 搜索 Google Drive 资源
+    zgd: "https://zhao.pp.ua/?q=%@",
     // DuckDuckGo
-    "ddg": "https://duckduckgo.com/?ia=about&q=%@",
+    ddg: "https://duckduckgo.com/?ia=about&q=%@",
     // Google
-    "gl": "https://www.google.com/search?q=%@",
-    "@default": "gl"
-}
+    gl: "https://www.google.com/search?q=%@",
+    "@default": "gl",
+};
 
-let commands = Object.keys(engineData)
-let url = $request.url
-let keyword = url.match(/duckduckgo.com\/\?q=([^&]+)/)
+let commands = Object.keys(engineData);
+let url = $request.url;
+let keyword = url.match(/duckduckgo.com\/\?q=([^&]+)/);
 if (keyword) {
-    keyword = keyword[1]
-    let patt = new RegExp(`^(${commands.join("|")})(\\+|%20)`, "g")
-    let command = keyword.match(patt)
+    keyword = keyword[1];
+    let patt = new RegExp(`^(${commands.join("|")})(\\+|%20)`, "g");
+    let command = keyword.match(patt);
     if (command) {
-        url = engineData[command[0].replace(/(\+|%20)/, "")].replace(/%@/, keyword.replace(command[0], ""))
+        url = engineData[command[0].replace(/(\+|%20)/, "")].replace(
+            /%@/,
+            keyword.replace(command[0], "")
+        );
     } else {
-        url = engineData[engineData["@default"]].replace(/%@/, keyword)
+        url = engineData[engineData["@default"]].replace(/%@/, keyword);
     }
-    const response={
-        status: "HTTP/1.1 302 Temporary Redirect",
+
+    const isQuanX = typeof $notify != "undefined";
+    const newstatus = isQuanX ? "HTTP/1.1 302 Temporary Redirect" : 302;
+    const redirect = {
+        status: newstatus,
         headers: {
             Location: url,
-        }
-    }
-
-    $done(response)
+        },
+    };
+    const resp = isQuanX ? redirect : { response: redirect };
+    $done(resp);
 } else {
-    $done({})
+    $done({});
 }
