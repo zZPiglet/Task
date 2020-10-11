cdc4db47960f51ab96fcd36475667953b93f50d7
zZPiglet <zZPiglet@users.noreply.github.com>
2020-10-12 00:04:29 +0800

    去除知乎 app 中的 source 信息保证跳转正确，为非法 URL 添加 http:// 头部。

--- noredirect.js
+++ noredirect.js
@@ -7,18 +7,22 @@ Quantumult X:
 ^https:\/\/links\.jianshu\.com\/go\?to= url script-echo-response https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
 ^https:\/\/www\.jianshu\.com\/go-wild\?ac=\d&url= url script-echo-response https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
 # 知乎
-^https:\/\/link\.zhihu\.com\/\?target= url script-echo-response https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
+^https:\/\/link\.zhihu\.com\/\?target=(https?)?(%3A|:)?(\/\/|%2F%2F)?(.*?)(&source.*)?$ url 302 http://$4
+# ^https:\/\/link\.zhihu\.com\/\?target= url script-echo-response https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
 # 微博
 ^https?:\/\/weibo\.cn\/sinaurl\?toasturl= url script-echo-response https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
 ^https?:\/\/(sinaurl|t)\.cn\/ url script-response-header https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
 
-Surge
+Surge:
+[URL Rewrite]
+^https:\/\/link\.zhihu\.com\/\?target=(https?)?(%3A|:)?(\/\/|%2F%2F)?(.*?)(&source.*)?$ http://$4 302
+
 [Script]
 # 简书
 Jianshunoredirect = type=http-request,pattern=^https:\/\/links\.jianshu\.com\/go\?to=,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
 Jianshunoredirect2 = type=http-request,pattern=^https:\/\/www\.jianshu\.com\/go-wild\?ac=\d&url=,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
 # 知乎
-Zhihunoredirect = type=http-request,pattern=^https:\/\/link\.zhihu\.com\/\?target=,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
+# Zhihunoredirect = type=http-request,pattern=^https:\/\/link\.zhihu\.com\/\?target=,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
 # 微博
 Weibonoredirect = type=http-request,pattern=^https?:\/\/weibo\.cn\/sinaurl\?toasturl=,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
 Weibonoredirect2 = type=http-response,pattern=^https?:\/\/(sinaurl|t)\.cn\/,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
@@ -39,13 +43,15 @@ if (oldurl.indexOf("links.jianshu.com/go") != -1) {
 } else if (oldurl.indexOf("www.jianshu.com/go") != -1) {
 	newurl = decodeURIComponent(jianshur2.exec(oldurl)[1]);
 } else if (oldurl.indexOf("link.zhihu.com/?target") != -1) {
-	newurl = decodeURIComponent(zhihur.exec(oldurl)[1]);
+	newurl = decodeURIComponent(zhihur.exec(oldurl)[1].replace(/&source=.*/, ""));
 } else if (oldurl.indexOf("weibo.cn/sinaurl?toasturl") != -1) {
 	newurl = decodeURIComponent(weibor.exec(oldurl)[1]);
 } else if (oldurl.indexOf("sinaurl.cn") != -1 || oldurl.indexOf("t.cn") != -1) {
 	let headers = $response.headers;
 	newurl = headers.Location;
 }
+
+newurl = newurl.indexOf("http") == 0 ? newurl : "http://" + newurl;
 
 const isQuanX = typeof $notify != "undefined";
 const newstatus = isQuanX ? "HTTP/1.1 302 Temporary Redirect" : 302;
