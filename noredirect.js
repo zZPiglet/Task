df7a7e9c2a6be42dfe3757102e4d0da150c488a0
zZPiglet <zZPiglet@users.noreply.github.com>
2020-10-12 08:00:40 +0800

    兼容国际版微博中另一种跳转链接

--- noredirect.js
+++ noredirect.js
@@ -10,7 +10,7 @@ Quantumult X:
 ^https:\/\/link\.zhihu\.com\/\?target=(https?)?(%3A|:)?(\/\/|%2F%2F)?(.*?)(&source.*)?$ url 302 http://$4
 # ^https:\/\/link\.zhihu\.com\/\?target= url script-echo-response https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
 # 微博
-^https?:\/\/weibo\.cn\/sinaurl\?toasturl= url script-echo-response https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
+^https?:\/\/weibo\.cn\/sinaurl\?(u|toasturl)= url script-echo-response https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
 ^https?:\/\/(sinaurl|t)\.cn\/ url script-response-header https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
 
 Surge:
@@ -24,7 +24,7 @@ Jianshunoredirect2 = type=http-request,pattern=^https:\/\/www\.jianshu\.com\/go-
 # 知乎
 # Zhihunoredirect = type=http-request,pattern=^https:\/\/link\.zhihu\.com\/\?target=,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
 # 微博
-Weibonoredirect = type=http-request,pattern=^https?:\/\/weibo\.cn\/sinaurl\?toasturl=,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
+Weibonoredirect = type=http-request,pattern=^https?:\/\/weibo\.cn\/sinaurl\?(u|toasturl)=,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
 Weibonoredirect2 = type=http-response,pattern=^https?:\/\/(sinaurl|t)\.cn\/,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
 
 [mitm]
@@ -34,7 +34,7 @@ hostname = links.jianshu.com, www.jianshu.com, link.zhihu.com, weibo.cn, t.cn, s
 const jianshur = /https:\/\/links\.jianshu\.com\/go\?to=(.*)/;
 const jianshur2 = /https:\/\/www\.jianshu\.com\/go-wild\?ac=\d&url=(.*)/;
 const zhihur = /https:\/\/link\.zhihu\.com\/\?target=(.*)/;
-const weibor = /https?:\/\/weibo\.cn\/sinaurl\?toasturl=(.*)/;
+const weibor = /https?:\/\/weibo\.cn\/sinaurl\?(u|toasturl)=(.*)/;
 
 const oldurl = $request.url;
 let newurl = "";
@@ -44,8 +44,8 @@ if (oldurl.indexOf("links.jianshu.com/go") != -1) {
 	newurl = decodeURIComponent(jianshur2.exec(oldurl)[1]);
 } else if (oldurl.indexOf("link.zhihu.com/?target") != -1) {
 	newurl = decodeURIComponent(zhihur.exec(oldurl)[1].replace(/&source=.*/, ""));
-} else if (oldurl.indexOf("weibo.cn/sinaurl?toasturl") != -1) {
-	newurl = decodeURIComponent(weibor.exec(oldurl)[1]);
+} else if (oldurl.indexOf("weibo.cn/sinaurl") != -1) {
+	newurl = decodeURIComponent(weibor.exec(oldurl)[2]);
 } else if (oldurl.indexOf("sinaurl.cn") != -1 || oldurl.indexOf("t.cn") != -1) {
 	let headers = $response.headers;
 	newurl = headers.Location;
