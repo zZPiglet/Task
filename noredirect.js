910bf25e3e5c815e01cf7e571d9aa7cc75cbbfe7
zZPiglet <zZPiglet@users.noreply.github.com>
2020-10-20 20:03:21 +0800

    新增并改进跳过微博重定向

--- noredirect.js
+++ noredirect.js
@@ -10,7 +10,8 @@ Quantumult X:
 ^https:\/\/link\.zhihu\.com\/\?target=(https?)?(%3A|:)?(\/\/|%2F%2F)?(.*?)(&source.*)?$ url 302 http://$4
 # ^https:\/\/link\.zhihu\.com\/\?target= url script-echo-response https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
 # 微博
-^https?:\/\/weibo\.cn\/sinaurl\?(u|toasturl)= url script-echo-response https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
+^https?:\/\/weibo\.cn\/sinaurl\?(.*&)?(u|toasturl|goto)= url script-echo-response https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
+^https:\/\/shop\.sc\.weibo\.com\/h5\/jump\/error\?(.*&)?url= url script-echo-response https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
 ^https?:\/\/(sinaurl|t)\.cn\/ url script-response-header https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
 
 Surge:
@@ -24,17 +25,19 @@ Jianshunoredirect2 = type=http-request,pattern=^https:\/\/www\.jianshu\.com\/go-
 # 知乎
 # Zhihunoredirect = type=http-request,pattern=^https:\/\/link\.zhihu\.com\/\?target=,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
 # 微博
-Weibonoredirect = type=http-request,pattern=^https?:\/\/weibo\.cn\/sinaurl\?(u|toasturl)=,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
-Weibonoredirect2 = type=http-response,pattern=^https?:\/\/(sinaurl|t)\.cn\/,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
+Weibonoredirect = type=http-request,pattern=^https?:\/\/weibo\.cn\/sinaurl\?(.*&)?(u|toasturl|goto)=,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
+Weibonoredircet2 = type=http-request,pattern=^https:\/\/shop\.sc\.weibo\.com\/h5\/jump\/error\?(.*&)?url=(.*),script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
+Weibonoredirect3 = type=http-response,pattern=^https?:\/\/(sinaurl|t)\.cn\/,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/noredirect.js
 
 [mitm]
-hostname = links.jianshu.com, www.jianshu.com, link.zhihu.com, weibo.cn, t.cn, sinaurl.cn
+hostname = links.jianshu.com, www.jianshu.com, link.zhihu.com, weibo.cn, t.cn, sinaurl.cn, shop.sc.weibo.com
 */
 
 const jianshur = /https:\/\/links\.jianshu\.com\/go\?to=(.*)/;
 const jianshur2 = /https:\/\/www\.jianshu\.com\/go-wild\?ac=\d&url=(.*)/;
 const zhihur = /https:\/\/link\.zhihu\.com\/\?target=(.*)/;
-const weibor = /https?:\/\/weibo\.cn\/sinaurl\?(u|toasturl)=(.*)/;
+const weibor = /https?:\/\/weibo\.cn\/sinaurl\?(.*&)?(u|toasturl|goto)=(.*?)(&.*)?$/;
+const weibor2 = /https:\/\/shop\.sc\.weibo\.com\/h5\/jump\/error\?(.*&)?url=(.*)/;
 
 const oldurl = $request.url;
 let newurl = "";
@@ -45,7 +48,9 @@ if (oldurl.indexOf("links.jianshu.com/go") != -1) {
 } else if (oldurl.indexOf("link.zhihu.com/?target") != -1) {
 	newurl = decodeURIComponent(zhihur.exec(oldurl)[1].replace(/&source=.*/, ""));
 } else if (oldurl.indexOf("weibo.cn/sinaurl") != -1) {
-	newurl = decodeURIComponent(weibor.exec(oldurl)[2]);
+	newurl = decodeURIComponent(weibor.exec(oldurl)[3]);
+} else if (oldurl.indexOf("shop.sc.weibo.com/h5/jump/error") != -1) {
+	newurl = decodeURIComponent(weibor2.exec(oldurl)[2]);
 } else if (oldurl.indexOf("sinaurl.cn") != -1 || oldurl.indexOf("t.cn") != -1) {
 	let headers = $response.headers;
 	newurl = headers.Location;
