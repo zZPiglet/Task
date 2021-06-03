bc19a4d94e983c5e5955cce636abf4b2d53e445d
zZPiglet <zZPiglet@users.noreply.github.com>
2021-06-03 19:02:31 +0800

    renew

---
+++ CMB/CMB_trip.js
@@ -0,0 +1,29 @@
+const $ = new compatibility();
+
+const cmb_trip = [
+    "cmbmobilebank://cmbls/functionjump?action=gofuncid&funcid=16335001&needlogin=true&cmb_app_trans_parms_start=here&ActGroupID=AGP202104151044012s5IENE3",
+    "cmbmobilebank://cmbls/functionjump?action=gofuncid&funcid=16335001&needlogin=true&cmb_app_trans_parms_start=here&ActGroupID=AGP20210528161839iYPHNZdp",
+    "cmbmobilebank://cmbls/functionjump?action=gofuncid&funcid=16335001&needlogin=true&cmb_app_trans_parms_start=here&ActGroupID=AGP2021041511590668bxa265",
+    "cmbmobilebank://cmbls/functionjump?action=gofuncid&funcid=16335001&needlogin=true&cmb_app_trans_parms_start=here&ActGroupID=AGP20210415114319Be1mnszZ",
+    "cmbmobilebank://cmbls/functionjump?action=gofuncid&funcid=16335001&needlogin=true&cmb_app_trans_parms_start=here&ActGroupID=AGP20210528162108rFfDzCfK",
+];
+
+!(async () => {
+    let i = new Date().getDay() - 1;
+    $.notify("招商银行-出游有招", "", "点击跳转去抢券啦", cmb_trip[i]);
+})().finally(() => $done());
+
+function compatibility() {
+    _isQuanX = typeof $task != "undefined";
+    _isLoon = typeof $loon != "undefined";
+    _isSurge = typeof $httpClient != "undefined" && !_isLoon;
+    this.read = (key) => {
+        if (_isQuanX) return $prefs.valueForKey(key);
+        if (_isLoon) return $persistentStore.read(key);
+    };
+    this.notify = (title, subtitle, message, url) => {
+        if (_isLoon) $notification.post(title, subtitle, message, url);
+        if (_isQuanX) $notify(title, subtitle, message, { "open-url": url });
+        if (_isSurge) $notification.post(title, subtitle, message, { url: url });
+    };
+}
