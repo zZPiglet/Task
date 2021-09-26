1ed852c2766dcc5ace4917c445bce9cc31dda25b
zZPiglet <zZPiglet@users.noreply.github.com>
2021-09-26 18:04:02 +0800

    Add icon and color, use it in argument.

--- asset/flushDNS.js
+++ asset/flushDNS.js
@@ -1,6 +1,8 @@
 /*
 [Script]
 flushDNS = type=generic,timeout=10,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/asset/flushDNS.js
+// use "icon" and "color" in "argument":
+// flushDNS = type=generic,timeout=10,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/asset/flushDNS.js,argument=icon=arrow.clockwise&color=#3d3d5b
 
 [Panel]
 flushDNS = script-name=flushDNS,update-interval=-1
@@ -11,10 +13,16 @@ flushDNS = script-name=flushDNS,update-interval=-1
     dnsCache = [...new Set(dnsCache.map((d) => d.server))].toString().replace(/,/g, "\n");
     await httpAPI("/v1/dns/flush");
     let delay = ((await httpAPI("/v1/test/dns_delay")).delay * 1000).toFixed(0);
-    $done({
+    let panel = {
         title: "Flush DNS",
         content: `delay: ${delay}ms${dnsCache ? `\nserver:\n${dnsCache}` : ""}`,
-    });
+    };
+    if (typeof $argument != "undefined") {
+        let arg = Object.fromEntries($argument.split("&").map((item) => item.split("=")));
+        panel.icon = arg.icon;
+        panel["icon-color"] = arg.color;
+    }
+    $done(panel);
 })();
 
 function httpAPI(path = "", method = "POST", body = null) {
